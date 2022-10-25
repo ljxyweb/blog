3e95e612175acea5ade8a3827b5b93fd30694ba1
zhouyan <yan.zhou05@geely.com>
2022-10-25 17:05:22 +0800

    文件夹修改

---
+++ demo/taro-to-uniapp/my1-uniapp/src/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view>
+      <text class="title">{{ title }}</text>
+      <button type="primary" @click="goToIndex" class="but">
+        跳转到taro分包项目
+      </button>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "taro与uniapp混合开发",
+    };
+  },
+  onLoad() {},
+  methods: {
+    goToIndex() {
+      uni.navigateTo({
+        url: "/taro/pages/index/index",
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+.but {
+  margin-top: 48rpx;
+}
+</style>
